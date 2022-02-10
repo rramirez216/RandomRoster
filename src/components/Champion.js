2662646a36f8f3e2d47810a0a6a22f16e65d7520
Ruben Ramirez <rramirez110911@gmail.com>
2022-02-09 18:56:18 -0800

    changes font size of the name and title of the champion. Also centers the text

--- src/components/Champion.js
+++ src/components/Champion.js
@@ -3,17 +3,27 @@ import styled from 'styled-components'
 
 const Champion = ({ champion: { id, name, title } }) => {
   return (
-    <div>
+    <Wrapper>
       <div>
         <img
           src={`http://ddragon.leagueoflegends.com/cdn/img/champion/splash/${id}_0.jpg`}
           alt=''
         />
       </div>
-      <p>{name}</p>
-      <p>{title}</p>
-    </div>
+      <Name>{name}</Name>
+      <Title>{title}</Title>
+    </Wrapper>
   )
 }
+
+const Wrapper = styled.div`
+  text-align: center;
+`
+const Name = styled.p`
+  font-size: 3rem;
+`
+const Title = styled.p`
+  font-size: 1.5rem;
+`
 
 export default Champion
