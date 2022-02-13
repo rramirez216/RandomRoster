73569c55c97e32d54f345caa7258efc0265bec1e
Ruben Ramirez <rramirez110911@gmail.com>
2022-02-12 20:45:43 -0800

    removes the extra div in the champion component.

--- src/components/Champion.js
+++ src/components/Champion.js
@@ -3,7 +3,7 @@ import styled from 'styled-components'
 
 const Champion = ({ champion: { id, name, title } }) => {
   return (
-    <Wrapper>
+    <>
       <div>
         <img
           src={`http://ddragon.leagueoflegends.com/cdn/img/champion/splash/${id}_0.jpg`}
@@ -12,13 +12,10 @@ const Champion = ({ champion: { id, name, title } }) => {
       </div>
       <Name>{name}</Name>
       <Title>{title}</Title>
-    </Wrapper>
+    </>
   )
 }
 
-const Wrapper = styled.div`
-  text-align: center;
-`
 const Name = styled.p`
   font-size: 3rem;
 `
