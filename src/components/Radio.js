11aa36007d0f9c50bc230767b525716be9c344e7
Ruben Ramirez <rramirez110911@gmail.com>
2022-03-02 19:46:19 -0800

    adds custom css styles to radio button component

--- src/components/Radio.js
+++ src/components/Radio.js
@@ -1,22 +1,72 @@
 import React from 'react'
 import styled from 'styled-components'
 
-const Checkbox = ({ handleRadio, value }) => {
+const Checkbox = ({ handleRadio, value, selected }) => {
   return (
     <Label htmlFor='radio'>
       {value}
-      <input
+      <Input
         type='radio'
         id='radio'
         name='radio'
-        onChange={() => {
-          handleRadio(value)
+        value={value}
+        checked={selected === value}
+        onChange={(event) => {
+          handleRadio(event.target.value)
         }}
       />
+      <Checkmark></Checkmark>
     </Label>
   )
 }
 
-const Label = styled.label``
+const Input = styled.input`
+  position: absolute;
+  opacity: 0;
+  cursor: pointer;
+`
+const Checkmark = styled.span`
+  position: absolute;
+  top: 0;
+  left: 0;
+  height: 25px;
+  width: 25px;
+  background-color: #eee;
+  border-radius: 50%;
+  &::after {
+    content: '';
+    position: absolute;
+    display: none;
+  }
+`
+const Label = styled.label`
+  display: block;
+  position: relative;
+  padding-left: 35px;
+  margin-bottom: 12px;
+  cursor: pointer;
+  font-size: 1.2rem;
+  -webkit-user-select: none;
+  -moz-user-select: none;
+  -ms-user-select: none;
+  user-select: none;
+  &:hover ${Input} ~ ${Checkmark} {
+    background-color: #ccc;
+  }
+  & ${Input}:checked ~ ${Checkmark} {
+    background-color: hsl(131, 27%, 49%);
+  }
+  & ${Input}:checked ~ ${Checkmark}::after {
+    display: block;
+  }
+  & ${Checkmark}::after {
+    top: 9px;
+    left: 9px;
+    width: 8px;
+    height: 8px;
+    border-radius: 50%;
+    background: white;
+  }
+`
 
 export default Checkbox
