a94962d53a5d7cb6934c4428cebaf1a6f6c43cbb
Ruben Ramirez <rramirez110911@gmail.com>
2022-03-15 18:54:43 -0700

    passes props show content and variants to and the opacity transition animation to the content inside champion component

--- src/components/Champion.js
+++ src/components/Champion.js
@@ -1,26 +1,42 @@
 import React from 'react'
+import '../index.css'
+import { motion } from 'framer-motion'
 import styled from 'styled-components'
 
 const Champion = ({
   champion: { id, name, title },
   skinNumber,
   handleSkinChange,
+  showContent,
+  variants,
 }) => {
   return (
-    <>
-      <div>
-        <Image
-          src={`http://ddragon.leagueoflegends.com/cdn/img/champion/splash/${id}_${skinNumber}.jpg`}
-          alt='splash art'
-          onClick={handleSkinChange}
-        />
-      </div>
-      <Name>{name}</Name>
-      <Title>{title}</Title>
-    </>
+    <Wrapper className='shadow' variants={variants} animate={'spin'}>
+      <InnerWrapper
+        variants={variants}
+        animate={showContent ? 'disappear' : 'reappear'}
+      >
+        <div>
+          <Image
+            src={`http://ddragon.leagueoflegends.com/cdn/img/champion/splash/${id}_${skinNumber}.jpg`}
+            alt='splash art'
+            onClick={handleSkinChange}
+          />
+        </div>
+        <Name>{name}</Name>
+        <Title>{title}</Title>
+      </InnerWrapper>
+    </Wrapper>
   )
 }
 
+const Wrapper = styled(motion.div)`
+  max-width: 450px;
+  text-align: center;
+  padding: 64px 32px;
+  border-radius: 16px;
+`
+const InnerWrapper = styled(motion.div)``
 const Image = styled.img`
   border-radius: 16px;
   cursor: pointer;
